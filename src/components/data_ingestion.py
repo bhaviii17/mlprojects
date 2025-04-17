95001b145fc16d6a943c2078f0d53bd5fbae62f9
bhaviii17 <munjalbhavya17@gmail.com>
2025-04-18 03:30:29 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,68 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
+
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df=pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Inmgestion of the data iss completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj=DataIngestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+
+
+
+
+
+
+
+#When to Use dataclass: If your class is primarily a container for 
+# data with no significant logic or methods other than variable storage, 
+# @dataclass is an excellent choice. It automatically generates the __init__, 
+# __repr__, __eq__, and other utility methods for you, reducing boilerplate. 
+##############################################################################
+#When to Use __init__: If your class includes additional methods, custom
+#initialization logic, or complex behavior that can't be handled by dataclass 
+# alone, manually defining __init__ is better.
+
